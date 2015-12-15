f8fc7600c3d1027d7f4269f94151ef341c77efbd
Mila Frerichs <mila.frerichs@gmail.com>
2015-12-15 14:20:54 +0100

    Automated commit at 2015-12-15 13:20:54 UTC by middleman-deploy 1.0.0

--- javascripts/all.js
+++ javascripts/all.js
@@ -65528,6 +65528,9 @@ demo.directive('filter', ['$rootScope', function($rootScope) {
   return {
     restrict: 'EA',
     require: '^babbage',
+    scope: {
+      filter: '@'
+    },
     link: function(scope, element, attrs, babbageCtrl) {
       //$rootScope.state.cut = ['test'];
       babbageCtrl.setState(scope.state);
--- javascripts/filterDirective.js
+++ javascripts/filterDirective.js
@@ -2,6 +2,9 @@ demo.directive('filter', ['$rootScope', function($rootScope) {
   return {
     restrict: 'EA',
     require: '^babbage',
+    scope: {
+      filter: '@'
+    },
     link: function(scope, element, attrs, babbageCtrl) {
       //$rootScope.state.cut = ['test'];
       babbageCtrl.setState(scope.state);
